24226752324f5fd993320895c3ba395b75693cac
Rodolfo <rodolfoespinosa01@gmail.com>
2023-10-14 23:45:47 -0400

    Created READ.md and started to fill it out.

--- db/query.sql
+++ db/query.sql
@@ -1,9 +1,27 @@
 
 
---  Get all employees by 
+--  Get all employees
+
+-- use employee_tracker;
+
+-- SELECT
+-- 	e.id,
+--     first_name,
+--     last_name,
+--     r.title AS title,
+--     d.name AS department,
+--     r.salary AS salary,
+--     manager
+--     FROM employees e
+--     JOIN roles r ON r.id = e.title_id
+--     JOIN departments d ON r.department_id = d.id
+--     ;
+
 
 use employee_tracker;
 
+
+-- get employee by employee number
 SELECT
 	e.id,
     first_name,
@@ -15,35 +33,5 @@ SELECT
     FROM employees e
     JOIN roles r ON r.id = e.title_id
     JOIN departments d ON r.department_id = d.id
+    where e.id = 1
     ;
-
-      -- Get all students
-
-      SELECT
-	e.id,
-    first_name,
-    last_name,
-    r.title AS title,
-    d.name AS department,
-    r.salary AS salary,
-    manager
-    FROM employees e
-    JOIN roles r ON r.id = e.title_id
-    JOIN departments d ON r.department_id = d.id
-    ;
-
-
-      -- Select
-      --   s.id,
-      --   s.first_name,
-      --   s.last_name,
-      --   c.title AS course_title,
-      --   u.name AS university,
-      --   CONCAT (study_group_leaders.first_name, ' ', study_group_leaders.last_name) AS study_group_leader
-      --   FROM students s
-      --     JOIN courses c
-      --       ON s.course_id = c.id
-      --       JOIN universities u
-      --       ON c.university_id = u.id
-      --       LEFT JOIN students study_group_leaders
-      --         ON s.study_group_leader_id = study_group_leaders.id;
